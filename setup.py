fccda9740cf952981ab492a1a955f9135b71bc05
Sunny Lam <sunny.lam@gmail.com>
2018-02-14 19:25:26 -0500

    revised docs

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
 	from distutils.core import setup
 
 config = {
-	'description': 'My Project',
-	'author': 'My Name',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
-	'author_email': 'My email',
+	'description': 'Prints and arranges a table using lists as columns.',
+	'author': 'Sunny Lam',
+	'url': 'https://github.com/sunnylam13/table_printer_020618_1',
+	'download_url': 'https://github.com/sunnylam13/table_printer_020618_1',
+	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
 	'packages': ['NAME'],
 	'scripts': [],
-	'name': 'projectname'
+	'name': 'Table Printer Using Lists'
 }
 
 setup(**config)
--- table_printer/table_printer_020618_1.py
+++ table_printer/table_printer_020618_1.py
@@ -5,19 +5,6 @@
 # STATUS:  unsolved
 
 # Table Printer
-# ABS: 234
-# Write a function named printTable() that takes a list of lists of strings and displays it in a well-organized table with each column right-justified. 
-# Assume that all the inner lists will contain the same number of strings.
-
-# Hint: Your code will first have to find the longest string in each of the inner lists so that the whole column can be wide enough to fit all the strings. 
-# You can store the maximum width of each column as a list of integers. 
-# The printTable() function can begin with colWidths = [0] * len(tableData), which will create a list containing the same number of 0 values as the number of inner lists in tableData. 
-# That way, colWidths[0] can store the width of the longest string in tableData[0], colWidths[1] can store the width of the longest string in tableData[1], and so on. 
-# You can then find the largest value in the colWidths list to find out what integer width to pass to the rjust() string method.
-
-# Resources
-# https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
-# https://stackoverflow.com/questions/12032214/print-new-output-on-same-line
 
 #####################################
 # TABLE DATA IMPORT
